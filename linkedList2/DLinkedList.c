bd0669caac372c8e559cec5780dc0e404d4a5588
HoonDongKang <d159123@naver.com>
2023-01-04 15:04:18 +0900

    linked list

--- linkedList2/DLinkedList.c
+++ linkedList2/DLinkedList.c
@@ -24,11 +24,14 @@ void FInsert(List * plist, LData data)
 void SInsert(List * plist, LData data)
 {
     Node * newNode = (Node *)malloc(sizeof(Node));
-    newNode->data=data;
-    
-    newNode->next=plist->head->next;
-    plist->head->next = newNode;
-
+    Node * pred = plist->head;
+    newNode -> data = data;
+    while(pred->next!=NULL && plist->comp(data,pred->next->data)!=0)
+    {
+        pred=pred->next;
+    }
+    newNode->next=pred->next;
+    pred->next = newNode;
     (plist->numOfData)++;
 }
 void LInsert(List * plist, LData data)
@@ -81,5 +84,7 @@ int LCount(List * plist)
 }
 
 void SetSortRule(List * plist, int(*comp)(LData d1, LData d2))
-{}
+{
+    plist->comp = comp;
+}
 
--- linkedList2/DLinkedListMain.c
+++ linkedList2/DLinkedListMain.c
@@ -2,12 +2,22 @@
 #include "DLinkedList.h"
 #include "DLinkedList.c"
 
+int WhoisPrecede(int d1, int d2)
+{
+    if(d1<d2)
+        return 0;
+    else    
+        return 1;
+}
+
 int main(void)
 {
     //initialize
     List list;
     int data;
     ListInit(&list);
+
+    SetSortRule(&list,WhoisPrecede);
 
     //insert
     LInsert(&list,11);LInsert(&list,11);
